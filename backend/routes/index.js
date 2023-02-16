f981401ee3b5f8448498c637fc4464c2f3b30e1e
Natalie Peel <natalie@Natalies-MacBook-Pro.local>
2023-02-15 21:28:10 -0700

    Added API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,12 @@
 // backend/routes/index.js
 const express = require("express");
 const router = express.Router();
+const apiRouter = require('./api');
 
-router.get("/hello/world", function (req, res) {
-  res.cookie("XSRF-TOKEN", req.csrfToken());
-  res.send("Hello World!");
-});
+router.use('/api', apiRouter);
+// router.get("/hello/world", function (req, res) {
+//   res.cookie("XSRF-TOKEN", req.csrfToken());
+//   res.send("Hello World!");
+// });
 
 module.exports = router;
