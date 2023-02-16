ac6357a58ef86d56ec7183848ca9b78871857ea4
Natalie Peel <natalie@Natalies-MacBook-Pro.local>
2023-02-16 16:19:23 -0700

    Add User model authentication Sequelize scopes and methods

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,57 +1,93 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+    toSafeObject() {
+        const { id, username, email } = this;
+        return { id, username, email };
+    }
+    validatePassword(password) {
+        return bcrypt.compareSync(password, this.hashedPassword.toString());
+    }
+    static getCurrentUserById(id) {
+        return User.scope("currentUser").findByPk(id);
+    }
+    static async login({ credential, password}) {
+        const { Op } = require('sequelize');
+        const user = await User.scope('loginUser').findOne({
+            where: {
+                [Op.or]: {
+                    username: credential,
+                    email:credential
+                }
+            }
+        });
+        if (user && user.validatePassword(password)) {
+            return await User.scope('currentUser').findByPk(user.id);
+        }
+    }
+    static async signup({ username, email, password }) {
+        const hashedPassword = bcrypt.hashSync(password);
+        const user = await User.create({
+            username,
+            email,
+            hashedPassword
+        });
+        return await User.scope('currentUser').findByPk(user.id);
+    }
     static associate(models) {
       // define association here
     }
   }
-  User.init({
-    username: {
+  User.init(
+    {
+      username: {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true,
         validate: {
-            len: [4,30],
-            isNotEmail(val) {
-                if(Validator.isEmail(val)) {
-                    throw new Error(`Username cannot be an email address.`)
-                }
+          len: [4, 30],
+          isNotEmail(val) {
+            if (Validator.isEmail(val)) {
+              throw new Error(`Username cannot be an email address.`);
             }
-        }
+          },
         },
-    email: {
+      },
+      email: {
         type: DataTypes.STRING,
-        allowNull:false,
+        allowNull: false,
         unique: true,
         validate: {
-            len: [3,30],
-            isEmail: true,
-        }
+          len: [3, 30],
+          isEmail: true,
         },
-    hashedPassword: {
+      },
+      hashedPassword: {
         type: DataTypes.STRING.BINARY,
         allowNull: false,
         validate: {
-            len: [60,60],
-        }
+          len: [60, 60],
         },
-    //   indexes: [
-    // {
-    //   unique: true,
-    //   fields: ['email']
-    // },
-    //   ]
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+      defaultScope: {
+        attributes: {
+          exclude: ["hassedPassword", "email", "createdAt", "updatedAt"],
+        },
+      },
+      scopes: {
+        currentUser: {
+          attributes: { exclude: ["hashedPassword"] },
+        },
+        loginUser: {
+          attributes: {},
+        },
+      },
+    }
+  );
   return User;
 };
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
-// BUG test route didnt work
-// router.post('/test', function(req, res) {
-//     res.json({ requestBody: req.body });
-// });
+router.post('/test', function(req, res) {
+    console.log(`ROUTE HIT`)
+    res.json({ requestBody: req.body });
+});
 
 module.exports = router;
