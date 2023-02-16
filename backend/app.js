e971abb98089bfc8f278fbaf268365adc8377208
Natalie Peel <natalie@Natalies-MacBook-Pro.local>
2023-02-15 21:04:47 -0700

    Initialize Express and Sequelize with CSRF protection

--- backend/app.js
+++ backend/app.js
@@ -1,46 +1,56 @@
-// const express = require('express');
-// require('express-async-errors');
-// const morgan = require('morgan'); // HTTP req logger
-// const cors = require('cors'); // security
-// const csurf = require('csurf'); // security, deprecated
-// const cookieParser = require('cookie-parser');
-// const { ValidationError } = require('sequelize');
-// const path = require('path')
+const express = require('express');
+require('express-async-errors');
+const morgan = require('morgan'); // HTTP req logger
+const cors = require('cors'); // security
+const csurf = require('csurf'); // security, deprecated
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
 
-// // require routers
+// require routers
+const routes = require('./routes');
 
-// const { environment } = require('./config');
-// const isProduction = environment === 'production';
+const { environment } = require('./config');
+const isProduction = environment === 'production';
 
-// const app = express();
-// app.use(morgan("dev"));
-// app.use(express.json());
-// app.use(express.urlencoded());
-// app.use(cookieParser());
-
-// /* --------------------------- Enable CORS --------------------------- */
-
-// app.use(cors({
-//     origin: 'http://localhost:5001',
-//     credentials: true
-// }));
-
-// /* --------------------------- Enable CRSF Protection --------------------------- */
-
-// app.use(csurf({
-//     cookie: {
-//         sameSite: 'strict',
-//         secure: true,
-//         httpOnly: true
-//     }
-// }));
-
-// /* ------------------------ Frontend Files -------------------------- */
+const app = express();
+app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(helmet());
+app.use(express.json());
 
 
-// /* ------------------------- API Endpoints -------------------------- */
+/* ---------------------------  CORS --------------------------- */
+// security middleware
+if (!isProduction) {
+// engable cors only in development
+    app.use(cors());
+};
+
+/* ------------------------ Helmet -------------------------- */
+// helps set a variety of headers to better secure your app
+app.use(
+  helmet.crossOriginResourcePolicy({
+    policy: "cross-origin",
+  })
+);
+
+/* ---------------------------  CRSF  --------------------------- */
+// The csurf middleware will add a _csrf cookie that is HTTP-only (can't be read by JavaScript) to any server response. It also adds a method on all requests (req.csrfToken) that will be set to another cookie (XSRF-TOKEN) later on
+
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true,
+    },
+  })
+);
+
+/* ------------------------ Frontend Files -------------------------- */
 
 
+/* ------------------------- API Endpoints -------------------------- */
+app.use(routes);
 
-// const port = 8000;
-// app.listen(port, () => console.log(`Server is listening on port ${port}`));
+module.exports = app;
