3378ef8b53b5444a12d7bb78942e5911473ff98e
Natalie Peel <natalie@Natalies-MacBook-Pro.local>
2023-02-16 21:36:19 -0700

    Made revisions to User authentication Express middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,38 @@
 const router = require('express').Router();
-const { restoreUser } = require('../../utils/auth.js');
-// restore stays top
+
+/*-------------------------Restore User stays here------------------------------*/
+
 // Connect restoreUser middleware to the API router
   // If current user session is valid, set req.user to the user in the database
   // If current user session is not valid, set req.user to null
+const { restoreUser } = require('../../utils/auth.js');
 router.use(restoreUser);
+
+router.get("/restore-user", (req, res) => {
+  return res.json(req.user);
+});
+/*------------------------------------------------------------------------------*/
+
+
+
+
+const { setTokenCookie } = require("../../utils/auth.js");
+const { User } = require("../../db/models");
+
+router.get("/set-token-cookie", async (_req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: "Demo-lition",
+    },
+  });
+  setTokenCookie(res, user);
+  return res.json({ user: user });
+});
+
+const { requireAuth } = require("../../utils/auth.js");
+router.get("/require-auth", requireAuth, (req, res) => {
+  return res.json(req.user);
+});
+
 
 module.exports = router;
--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -1,67 +1,68 @@
 const jwt = require('jsonwebtoken');
 const { jwtConfig } = require('../config');
 const { User } = require('../db/models');
-const user = require('../db/models/user');
 
 const { secret, expiresIn } = jwtConfig;
 
 // sert the JWT cookie after a user logs in or signs up
 // will expire according to .env file
 const setTokenCookie = (res, user) => {
-    const token = jwt.sign(
-        { data: user.toSafeObject() },
-        secret,
-        { expiresIn: parseInt(expiresIn) }
-    );
+  // Create the token.
+  const token = jwt.sign(
+    { data: user.toSafeObject() },
+    secret,
+    { expiresIn: parseInt(expiresIn) } // 604,800 seconds = 1 week
+  );
 
-    const isProduction = process.env.NODE_ENV === "production";
+  const isProduction = process.env.NODE_ENV === "production";
 
-    res.cookie('token', token, {
-        maxAge: expiresIn * 1000, //in milliseconds
-        httpOnly: true,
-        secure: isProduction,
-        sameSite: isProduction && "Lax"
-    });
+  // Set the token cookie
+  res.cookie("token", token, {
+    maxAge: expiresIn * 1000, // maxAge in milliseconds
+    httpOnly: true,
+    secure: isProduction,
+    sameSite: isProduction && "Lax",
+  });
 
-    return token;
+  return token;
 };
 
 // certain routes require reidentification of current user
 // will connect to API router
 const restoreUser = (req, res, next) => {
-    // token parsed from cookies
-    const { token } = req.cookies;
-    req.user = null;
+  // token parsed from cookies
+  const { token } = req.cookies;
+  req.user = null;
 
-    return jwt.verify(token, secret, null, async (err, jwtPayload) => {
-        if (err) {
-            return next();
-        }
+  return jwt.verify(token, secret, null, async (err, jwtPayload) => {
+    if (err) {
+      return next();
+    }
 
-        try {
-            const { id } = jwtPayload.data;
-            req.user = await User.scope('currentUser').findByPk(id);
-        } catch(e) {
-            res.clearCookie('token');
-            return next();
-        }
-        if (!req.user) res.clearCookie('token');
+    try {
+      const { id } = jwtPayload.data;
+      req.user = await User.scope("currentUser").findByPk(id);
+    } catch (e) {
+      res.clearCookie("token");
+      return next();
+    }
 
-        return next();    
-    });
-}
+    if (!req.user) res.clearCookie("token");
+
+    return next();
+  });
+};
 
 // requiring a session user to be authenticated before accessing a route
 // connect directly to route handlers that user needs to be logged in
 const requireAuth = function (req, _res, next) {
-    if (req.user) return next();
+  if (req.user) return next();
 
-    const err = new Error('Authentication required');
-    err.title = 'Authentication required';
-    err.errors = { message: "Authentication required" };
-    err.status = 401;
-    return next(err);
-}
-
+  const err = new Error("Authentication required");
+  err.title = "Authentication required";
+  err.errors = { message: "Authentication required" };
+  err.status = 401;
+  return next(err);
+};
 
 module.exports = { setTokenCookie, restoreUser, requireAuth };
