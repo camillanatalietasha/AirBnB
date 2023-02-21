22f0f8b704ff5f1061bb12edd02e9523c20081e4
Natalie Peel <natalie@Natalies-MacBook-Pro.local>
2023-02-20 22:44:33 -0700

    SpotImage table migration and model complete

---
+++ backend/db/models/spotimage.js
@@ -0,0 +1,36 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class SpotImage extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // Spot images are related to one spot - association
+      SpotImage.belongsTo(models.Spot, { foreignKey: "spotId" });
+    }
+  }
+  SpotImage.init({
+    spotId: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        },
+    imgUrl: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        },
+    isPreview: {
+        allowNull: false,
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+        },
+  }, {
+    sequelize,
+    modelName: 'SpotImage',
+  });
+  return SpotImage;
+};
