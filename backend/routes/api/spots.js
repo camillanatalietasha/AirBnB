b3170dcbcd0ba97e9ed33ee5bf44f87f4179f786
Natalie Peel <natalie@Natalies-MacBook-Pro.local>
2023-02-22 11:27:05 -0700

    Get details for spot by id route is done

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -63,5 +63,59 @@ router.get('/current', requireAuth, async (req, res) => {
   res.status(200).json({ Spots });
 })
 
+router.get("/:spotId", async (req, res) => {
+  // extract spotId from params
+  const spotId = req.params.spotId;
+  // retrieve spot details and convert to JSON object
+  const getSpot = await Spot.findByPk(spotId);
+  const allSpotDetails = getSpot.toJSON();
+
+  // get and set key for num reviews  
+  await Review.count({ where: { "spotId": spotId}})
+    .then(c => { allSpotDetails.numReviews = c });
+  await Review.sum('stars', { where: { "spotId": spotId }})
+    .then(s => { allSpotDetails.avgStarRating = s / allSpotDetails.numReviews });
+  await SpotImage.findAll({where: { spotId: spotId }})
+    .then(i => {allSpotDetails.SpotImages = i});
+
+  res.status(200).json(allSpotDetails);
+});
+
+
+
+// -------------------> saving for refactoring
+// router.get("/:spotId", async (req, res) => {
+//   // extract spotId from params
+//   const spotId = req.params.spotId;
+//   // retrieve spot details (with reviews and images)
+//   const getSpot = await Spot.findByPk(spotId, {
+//     include: [
+//       {
+//         model: Review,
+//         attributes: ["stars"], // rename this attribute, count later
+//       },
+//       {
+//         model: SpotImage,
+//       },
+//     ],
+//   });
+//   // convert to JSON object
+//   const allSpotDetails = getSpot.toJSON();
+//   // iterate to total numReviews and sum all ratings
+// //   let ratingTotal = 0;
+// //   let numReviews = 0
+//   allSpotDetails.Reviews.forEach((rev) => {
+//     allSpotDetails.numReviews += 1;
+//     allSpotDetails.avgStarRating += rev.stars;
+//   });
+//   // calculate avg and remove reviews obj
+//   allSpotDetails.avgStarRating = allSpotDetails.avgStarRating / allSpotDetails.numReviews;
+//   delete allSpotDetails.Reviews;
+
+
+
+//   res.status(200).json({allSpotDetails});
+// });
+
 
 module.exports = router;
