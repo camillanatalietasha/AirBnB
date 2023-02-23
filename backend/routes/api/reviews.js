e9152e9976e8090c42f73255482e531babd4e4f2
Natalie Peel <natalie@Natalies-MacBook-Pro.local>
2023-02-22 20:51:13 -0700

    Typo fix on GET user reviews

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -10,7 +10,7 @@ const { sequelize, Op } = require("sequelize");
 
 /*================================== routes ===============================*/
 
-router.get('/', [requireAuth, restoreUser ], async (req, res) => {
+router.get('/current', [requireAuth, restoreUser ], async (req, res) => {
   const userId = req.user.id;
 
   const pagination = paginator(req, res);
