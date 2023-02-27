ab6b9a31c26950eb9d5fa0b6e27ea0ed10de5b2a
Natalie <cnapeel@gmail.com>
2023-02-26 22:08:45 -0700

    validation error fix

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -13,14 +13,10 @@ const handleValidationErrors = (req, _res, next) => {
       .array()
       .forEach((error) => (errors[error.param] = error.msg));
 
-    // const err = Error("Bad request.");
-    // err.errors = errors;
-    // err.status = 400;
-    // err.title = "Bad request.";
-    res.status(401)
-    const err = {};
-    err.message = "Invalid credentials",
-    err.status = 401
+    const err = Error("Validation Error");
+    err.errors = errors;
+    err.status = 400;
+    err.title = "Bad request.";
     next(err);
   }
   next();
