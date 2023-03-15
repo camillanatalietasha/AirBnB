943540a4362a2ef0d36d37f12843aca03a7d2a03
Natalie <cnapeel@gmail.com>
2023-03-14 23:57:57 -0600

    Added to Create Spot form - still incomplete

--- frontend/src/components/CreateSpot/index.js
+++ frontend/src/components/CreateSpot/index.js
@@ -20,8 +20,136 @@ function CreateSpot () {
   const [price, setPrice] = useState("");
   const [description, setDescription] = useState("");
   const [previewImage, setPreviewImage] = useState("");
-  const [image1, setImage1] = useState("");
-  const [image2, setImage2] = useState("");
-  const [image3, setImage3] = useState("");
-  const [image4, setImage4] = useState("");
+  const [imageOne, setImageOne] = useState("");
+  const [imageTwo, setImageTwo] = useState("");
+  const [imageThree, setImageThree] = useState("");
+  const [imageFour, setImageFour] = useState("");
+
+  const [validationErrors, setValidationErrors] = useState();
+  const [submitted, setSubmitted] = useState();
+
+  useEffect(() => {
+    const spot = {
+      address,
+      city,
+      state,
+      country,
+      lat,
+      lng,
+      name,
+      price,
+      description,
+      previewImage,
+      imageOne,
+      imageTwo,
+      imageThree,
+      imageFour,
+    };
+    setValidationErrors()
+    // TODO create spot form validation
+  })
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    setSubmitted(true);
+
+    const submitObj = {
+      newSpot: {
+        address,
+        city,
+        state,
+        country,
+        lat,
+        lng,
+        name,
+        price,
+        description,
+      },
+      previewImage,
+      images: {
+        imageOne,
+        imageTwo,
+        imageThree,
+        imageFour
+      },
+    };
+  // dispatch for new spot id
+  // reset fields?
+  // go to new spot page
+  }
+
+  return (
+    <div>
+      <form id="create-spot-form">
+        <h1>Create a New Spot</h1>
+        <div>
+          <h3>Where's your place located?</h3>
+          <p>
+            Guests will only get your exact address once they booked a
+            reservation.
+          </p>
+        </div>
+        <label>
+          Country
+          <input
+            name="country"
+            type="text"
+            placeholder="..."
+            value={country}
+            onChange={(e) => setCountry(e.target.value)}
+          />
+        </label>
+        <label>
+          Street Address
+          <input
+            name="address"
+            type="text"
+            placeholder="..."
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          />
+        </label>
+        <label>
+          City
+          <input
+            name="city"
+            type="text"
+            placeholder="..."
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
+        </label>
+        <label>
+          State
+          <input
+            name="state"
+            type="text"
+            placeholder="..."
+            value={state}
+            onChange={(e) => setState(e.target.value)}
+          />
+        </label>
+        <label>
+          <div>
+            <h3>Describe your place to guests</h3>
+            <p>
+              Mention the best features of your space, any special amentities
+              like fast wif or parking, and what you love about the
+              neighborhood.
+            </p>
+          </div>
+          <textarea
+            name="description"
+            rows="10"
+            placeholder="..."
+            value={ description }
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </label>
+      </form>
+    </div>
+  );
+
+
+
 }
