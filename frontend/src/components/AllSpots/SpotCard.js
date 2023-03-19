b560686d7cf4f0ec783cbb0e6ad9fbc70f264764
Natalie <cnapeel@gmail.com>
2023-03-19 01:49:06 -0600

    testing image working - changed src data syntax

--- frontend/src/components/AllSpots/SpotCard.js
+++ frontend/src/components/AllSpots/SpotCard.js
@@ -9,17 +9,19 @@ function SpotCard({ spot }) {
   const history = useHistory();
 
   return (
-    <div className='spot-card'>
+    <div className="spot-card">
       <Link key={spot.id} to={`/spots/${spot.id}`}>
-        <img className='spot-card-img' src={`${spot.previewImage.imgUrl}`} alt={spot.name} />
-        <div className='spot-preview-info'>
+        <div className="spot-card-img">
+          <img src={`${spot.previewImage}`} alt={spot.name} />
+        </div>
+        <div className="spot-preview-info">
           <h3>{spot.name}</h3>
           <h3>{spot.price} per night</h3>
-          <span className='spot-card-ratings'>{spot.avgRating}</span>
+          <span className="spot-card-ratings">{spot.avgRating}</span>
         </div>
       </Link>
     </div>
-  )
+  );
 }
 
 export default SpotCard;
